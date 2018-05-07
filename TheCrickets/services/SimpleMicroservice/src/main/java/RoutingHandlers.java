de2208c475de9f56ebd38a6342512ed3682425e6
Covaliu Lucian <luciancovaliu41@gmail.com>
2018-05-07 15:50:53 +0300

    Fixed Access-Control-Allow-Origin

--- TheCrickets/services/SimpleMicroservice/src/main/java/RoutingHandlers.java
+++ TheCrickets/services/SimpleMicroservice/src/main/java/RoutingHandlers.java
@@ -1,5 +1,6 @@
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
+import io.undertow.util.HttpString;
 import microservice.JsonUtilities;
 
 public class RoutingHandlers
@@ -7,6 +8,7 @@ public class RoutingHandlers
 
     public static void notFoundHandler(HttpServerExchange exchange)
     {
+        exchange.getResponseHeaders().put(HttpString.tryFromString("Access-Control-Allow-Origin"), "*");
         exchange.setStatusCode(404);
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
         exchange.getResponseSender().send("Page Not Found!!");
@@ -14,6 +16,7 @@ public class RoutingHandlers
 
     public static void testHandler(HttpServerExchange exchange)
     {
+        exchange.getResponseHeaders().put(HttpString.tryFromString("Access-Control-Allow-Origin"), "*");
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
         String n1 = exchange.getQueryParameters().get("n1").getFirst();
         String n2 = exchange.getQueryParameters().get("n2").getFirst();
--- TheCrickets/services/mapper/src/main/java/RoutingHandlers.java
+++ TheCrickets/services/mapper/src/main/java/RoutingHandlers.java
@@ -1,5 +1,7 @@
 import io.undertow.server.HttpServerExchange;
+import io.undertow.server.handlers.OriginHandler;
 import io.undertow.util.Headers;
+import io.undertow.util.HttpString;
 import microservice.JsonUtilities;
 import microservice.ServerDetails;
 
@@ -9,6 +11,7 @@ public class RoutingHandlers
 
     public static void notFoundHandler(HttpServerExchange exchange)
     {
+        exchange.getResponseHeaders().put(HttpString.tryFromString("Access-Control-Allow-Origin"), "*");
         exchange.setStatusCode(404);
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
         exchange.getResponseSender().send("Page Not Found!!");
@@ -16,6 +19,8 @@ public class RoutingHandlers
 
     public static void mapHandler(HttpServerExchange exchange)
     {
+        exchange.getResponseHeaders().put(HttpString.tryFromString("Access-Control-Allow-Origin"), "*");
+        exchange.getResponseHeaders().put(Headers.ORIGIN, "*");
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
         String name = exchange.getQueryParameters().get("name").getFirst();
         ServerDetails details = Server.map.getDetails(name);
