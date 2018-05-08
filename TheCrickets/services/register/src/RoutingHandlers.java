9557b14d3736fbf3eb524454045812f6aa0938bf
Emilia Sfirnaciuc <sfirnaciuc_emilia@yahoo.com>
2018-05-09 02:13:44 +0300

    Register : Adding an account to the database.

--- TheCrickets/services/register/src/RoutingHandlers.java
+++ TheCrickets/services/register/src/RoutingHandlers.java
@@ -34,9 +34,19 @@ public class RoutingHandlers
         String mailRegex = result.email;
         if(mailRegex.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])") ) {
 
-            exchange.setStatusCode(200);
-            JsonUtilities.sendJson(exchange, "Email correct!");
 
+
+            CRUD_operations operations = new CRUD_operations();
+            if(operations.checkUserExists(result.email, result.password)) {
+                exchange.setStatusCode(406);
+                JsonUtilities.sendJson(exchange, "Aceste cont exista deja!");
+            }
+            else {
+
+                operations.insertUser("", "", result.email, result.password);
+                exchange.setStatusCode(200);
+                JsonUtilities.sendJson(exchange, "Ati fost inregistrat!");
+            }
         }
 
         else
