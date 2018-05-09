0e1d99d2d561640af0d7dc6616d4e1c85c1e9d83
Marțincu Petru <martincu.petru@gmail.com>
2018-05-10 01:24:34 +0300

    Changed createTables.sql

--- TheCrickets/database/MySQL_CRUD/src/Main.java
+++ TheCrickets/database/MySQL_CRUD/src/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         CRUD_operations operations = new CRUD_operations();
         /*operations.deleteAllUsers();
-        operations.insertUser("Petru", "Martincu", "martincu.petru@gmail.com", "pass123", new Date(2018,5,9), "Adress_1");
-        operations.insertUser("Emilia", "Sfirnaciuc", "emilia.sfirnaciuc@gmail.com", "2018", new Date(2018, 5,9), "Adress_1");
-        operations.insertUser("Lucian", "Covaliu", "lucian.covaliu@gmai.com", "abcdef", new Date(2018, 5,9), "Adress_1");
-        operations.insertUser("Andreea", "Dascalu", "andreea.dascalu@gmail.com", "12345", new Date(2018, 5,9), "Adress_1");
+        operations.insertUser("Petru", "Martincu", "martincu.petru@gmail.com", "pass123", new Date(2018,5,9), "0783658395");
+        operations.insertUser("Emilia", "Sfirnaciuc", "emilia.sfirnaciuc@gmail.com", "2018", new Date(2018, 5,9), "0965597739");
+        operations.insertUser("Lucian", "Covaliu", "lucian.covaliu@gmai.com", "abcdef", new Date(2018, 5,9), "0955839563");
+        operations.insertUser("Andreea", "Dascalu", "andreea.dascalu@gmail.com", "12345", new Date(2018, 5,9), "0754763849");
         System.out.println(operations.checkUserExists("martincu.petru@gmail.com", "pass123"));
         System.out.println(operations.readUserData("martincu.petru@gmail.com").toString());
         operations.insertUserByEmailPassword("martincu.petru@gmail.com", "12345");
