7e1e0f20926c8ec4c58bcd5174e4ccb670d78341
Marțincu Petru <martincu.petru@gmail.com>
2018-05-09 14:08:11 +0300

    Added sessionID table

--- TheCrickets/database/createTables.sql
+++ TheCrickets/database/createTables.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS sessionID;
+
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
@@ -8,3 +10,25 @@ CREATE TABLE users
   email VARCHAR(64),
   password VARCHAR(1024)
 );
+
+CREATE TABLE sessionID
+(
+  id INT(8) AUTO_INCREMENT PRIMARY KEY,
+  userID INT(8),
+  expiringDate TIMESTAMP,
+  FOREIGN KEY (userID)
+    REFERENCES users(id)
+  ON DELETE CASCADE
+);
+
+DROP TRIGGER IF EXISTS insertTime;
+
+CREATE TRIGGER insertTime
+  BEFORE INSERT ON sessionID
+    FOR EACH ROW
+      SET NEW.expiringDate = adddate(NOW(), 1);
+
+/*INSERT INTO users(firstName, lastName, email, password) VALUE ('Martincu', 'Petru', 'martincu.petru@gmail.com', '12345');
+INSERT INTO sessionID(userID) VALUE (1);
+SELECT * FROM sessionID;
+SELECT * FROM users;*/
