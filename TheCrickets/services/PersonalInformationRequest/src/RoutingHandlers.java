c767a01840adaf55b5dc544bf9d825737a0a71cf
Covaliu Lucian <luciancovaliu41@gmail.com>
2018-05-09 12:03:44 +0300

    Javascript connection

--- TheCrickets/services/PersonalInformationRequest/src/RoutingHandlers.java
+++ TheCrickets/services/PersonalInformationRequest/src/RoutingHandlers.java
@@ -3,6 +3,7 @@ import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 import io.undertow.util.HttpString;
 import microservice.JsonUtilities;
+
 import java.util.Deque;
 
 public class RoutingHandlers
@@ -14,7 +15,7 @@ public class RoutingHandlers
         exchange.setStatusCode(404);
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
         exchange.getResponseSender().send("Page Not Found!!");
- }
+    }
 
     public static void userHandler(HttpServerExchange exchange)
     {
@@ -23,19 +24,27 @@ public class RoutingHandlers
         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
 
         exchange.setStatusCode(200);
-        User result = JsonUtilities.parseJson(exchange, new TypeReference<User>(){});
+        User result = JsonUtilities.parseJson(exchange, new TypeReference<User>()
+        {
+        });
         System.out.println(result);
 
+        if (result.email == null || result.firstName == null || result.lastName == null)
+        {
+            exchange.setStatusCode(400);
+            JsonUtilities.sendJson(exchange, "Email or lastName or firstName incorrect!");
+            return;
+        }
 
         String mailRegex = result.email;
         String lastNameRegex = result.lastName;
         String firstNameRegex = result.firstName;
 
-        if(mailRegex.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])") && lastNameRegex.matches("[a-zA-Z ]{2,30}") && firstNameRegex.matches("[a-zA-Z ]{2,30}"))
+        if (mailRegex.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])") && lastNameRegex.matches("[a-zA-Z ]{2,30}") && firstNameRegex.matches("[a-zA-Z ]{2,30}"))
         {
             CRUD_operations operations = new CRUD_operations();
             User user = operations.readUserData(result.email);
-            if(user.id == 0)
+            if (user.id == 0)
             {
                 exchange.setStatusCode(401);
                 JsonUtilities.sendJson(exchange, "User-ul nu exista!");
@@ -49,11 +58,11 @@ public class RoutingHandlers
             }
 
         }
-        /*else
+        else
         {
             exchange.setStatusCode(401);
             JsonUtilities.sendJson(exchange, "Email or lastName or firstName incorrect!");
-        }*/
+        }
 
     }
 }
