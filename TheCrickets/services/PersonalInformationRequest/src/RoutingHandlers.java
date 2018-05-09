af7a954308baf2cf1ff6e23e90e74f32e85865d8
Covaliu Lucian <luciancovaliu41@gmail.com>
2018-05-09 11:55:45 +0300

    Fixed RegEx error

--- TheCrickets/services/PersonalInformationRequest/src/RoutingHandlers.java
+++ TheCrickets/services/PersonalInformationRequest/src/RoutingHandlers.java
@@ -31,7 +31,7 @@ public class RoutingHandlers
         String lastNameRegex = result.lastName;
         String firstNameRegex = result.firstName;
 
-        //if(mailRegex.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])") && lastNameRegex.matches("/^[a-zA-Z ]{2,30}$/") && firstNameRegex.matches("/^[a-zA-Z ]{2,30}$/"))
+        if(mailRegex.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])") && lastNameRegex.matches("[a-zA-Z ]{2,30}") && firstNameRegex.matches("[a-zA-Z ]{2,30}"))
         {
             CRUD_operations operations = new CRUD_operations();
             User user = operations.readUserData(result.email);
