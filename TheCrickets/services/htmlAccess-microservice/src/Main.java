ccd541abd240b12039121105ad9311921ceb250b
Marțincu Petru <martincu.petru@gmail.com>
2018-06-08 20:58:29 +0300

    Added new microservice
    It will be used to open pages using the localhost thing

---
+++ TheCrickets/services/htmlAccess-microservice/src/Main.java
@@ -0,0 +1,20 @@
+import io.undertow.server.HttpHandler;
+import io.undertow.server.RoutingHandler;
+import microservice.Server;
+
+import java.security.InvalidParameterException;
+
+public class Main
+{
+    private static final HttpHandler ROUTES = new RoutingHandler()
+            .get("/login", RoutingHandlers::testHandler)
+            .setFallbackHandler(RoutingHandlers::notFoundHandler);
+
+    public static void main(String[] args)
+    {
+        if (args.length < 1)
+            throw new InvalidParameterException();
+        System.out.println("Starting server number 2...");
+        Server.start(Integer.parseInt(args[0]), ROUTES);
+    }
+}
