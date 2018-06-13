6593064d74380e10fa6deb19483a793f4cb1245a
Marțincu Petru <martincu.petru@gmail.com>
2018-06-13 12:07:13 +0300

    Contact page fully functional

---
+++ TheCrickets/services/htmlAccess-microservice/src/main/java/RoutingHandlersFive.java
@@ -0,0 +1,39 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+import io.undertow.util.HttpString;
+import microservice.JsonUtilities;
+
+public class RoutingHandlersFive {
+
+    public static String SENDTO = "martincu.petru@gmail.com";
+
+    public static void userHandler(HttpServerExchange exchange)
+    {
+
+
+        exchange.getResponseHeaders().put(HttpString.tryFromString("Access-Control-Allow-Origin"), "*");
+
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+
+        exchange.setStatusCode(200);
+
+        Contact result = JsonUtilities.parseJson(exchange, new TypeReference<Contact>() {});
+
+        Email.doSendMail("theCrikets2@gmail.com", "!@#$%678", SENDTO,"[The Crickets - comments]",
+                "Dear Administrator,\n" +
+                        "\n" +
+                        "Mr/Mrs " + result.getFirstName() + " " + result.getLastName() + " sent you a comment.\n" +
+                        "Below you can read his comment:\n" +
+                        "\n" +
+                        "  \"" + result.getComment() + "\"\n" +
+                        "\n" +
+                        "\n" +
+                        "You can contact him/her using the email adress: " + result.getEmail() + "\n" +
+                        "\n" +
+                        "This email was automatically sent by htmlAccess-Microservice\n" +
+                        "Have a wonderful day!"
+        );
+
+    }
+}
