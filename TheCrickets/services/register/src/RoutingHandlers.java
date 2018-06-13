344a08f1bb24eab8a1f8e3cd0e35d8606dfc6469
Emilia Sfirnaciuc <sfirnaciuc_emilia@yahoo.com>
2018-06-13 22:56:16 +0300

    Documentatia generata din cod

--- TheCrickets/services/register/src/RoutingHandlers.java
+++ TheCrickets/services/register/src/RoutingHandlers.java
@@ -8,7 +8,9 @@ import java.util.Deque;
 public class RoutingHandlers
 {
 
-
+    /**
+     *  Checking if the page exist
+     */
     public static void notFoundHandler(HttpServerExchange exchange)
     {
         exchange.setStatusCode(404);
@@ -21,7 +23,9 @@ public class RoutingHandlers
 
     }
 
-
+    /**
+     * Checking if the email and the password is correct and adding them
+     */
     public static void UserPostHandler(HttpServerExchange exchange)
     {
         exchange.getResponseHeaders().put(HttpString.tryFromString("Access-Control-Allow-Origin"), "*");
@@ -32,17 +36,26 @@ public class RoutingHandlers
 
 
         String mailRegex = result.email;
+        /**
+         * Checking if the email is correct
+         */
         if(mailRegex.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])") ) {
 
 
 
             CRUD_operations operations = new CRUD_operations();
+/**
+ * Checking if the email and the password doesn't exist already
+ */
             if(operations.checkUserExists(result.email, result.password)) {
                 exchange.setStatusCode(406);
                 JsonUtilities.sendJson(exchange, "Aceste cont exista deja!");
             }
             else {
-
+/**
+ *
+ *  adding the email and the password
+ */
                 operations.insertUserByEmailPassword(result.email, result.password);
                 exchange.setStatusCode(200);
                 JsonUtilities.sendJson(exchange, "Ati fost inregistrat!");
