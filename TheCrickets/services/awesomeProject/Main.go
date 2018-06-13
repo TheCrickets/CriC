f236ff9d6611f140ae3fbb45bf8948867864883b
Marțincu Petru <martincu.petru@gmail.com>
2018-06-14 02:43:26 +0300

    Go finds in static the files

--- TheCrickets/services/awesomeProject/Main.go
+++ TheCrickets/services/awesomeProject/Main.go
@@ -4,14 +4,7 @@ import (
 	"net/http"
 )
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	// allow cross domain AJAX requests
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-}
-
 func main() {
-	fs := http.FileServer(http.Dir("D:\\CriC\\CriC\\TheCrickets\\viewsBackup\\"))
-	http.Header.Add(http.Header{}, "Access-Control-Allow-Origin", "*")
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.ListenAndServe(":3000", nil)
 }
